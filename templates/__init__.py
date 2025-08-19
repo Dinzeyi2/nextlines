0200fd77c91c3bb1aae2cfb75ced2b5150c95f12
Dinzeyi2 <133053311+Dinzeyi2@users.noreply.github.com>
2025-08-19 01:57:10 -0400

    Add additional pandas and sklearn templates with tests

--- templates/__init__.py
+++ templates/__init__.py
@@ -9,11 +9,19 @@ TASK_INVENTORY = {
     "data_loading": {
         "read_csv": pandas.TEMPLATES.get("read_csv"),
     },
+    "dataframe_operations": {
+        "dropna": pandas.TEMPLATES.get("dropna"),
+        "merge": pandas.TEMPLATES.get("merge"),
+        "groupby": pandas.TEMPLATES.get("groupby"),
+    },
     "preprocessing": {
         "standard_scaler": sklearn.TEMPLATES.get("standard_scaler"),
+        "train_test_split": sklearn.TEMPLATES.get("train_test_split"),
+        "pca": sklearn.TEMPLATES.get("pca"),
     },
     "model_training": {
         "logistic_regression": sklearn.TEMPLATES.get("logistic_regression"),
+        "random_forest": sklearn.TEMPLATES.get("random_forest"),
     },
 }
 
